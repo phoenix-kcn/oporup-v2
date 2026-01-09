a10dc2032071f696bf8e7b54a2d738b2cb48f943
Shahadat Hossain <sht.kcn@gmail.com>
2026-01-10 05:42:08 +0600

    Removing contact us button from urls.py file

--- store/urls.py
+++ store/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('category/<str:foo>/', views.category, name='category'),
     path('category_summary/', views.category_summary, name='category_summary'),
-    
     path('search/', views.search, name='search'),
 ] 
